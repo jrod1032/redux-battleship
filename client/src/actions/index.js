d2fa33e5f4689bdcedd80ef3e268b7182334a9a2
Jarrod Powell <jpowell1032@gmail.com>
2018-05-06 18:18:46 -0700

    Add addShip and destroyShip actions, and reducer cases

---
+++ client/src/actions/index.js
@@ -0,0 +1,39 @@
+
+export const START_GAME = 'START_GAME';
+export const ADD_SHIP = 'ADD_SHIP';
+export const DESTROY_SPOT = 'DESTROY_SPOT';
+export const CHOOSE_SHIP = 'CHOOSE_SHIP';
+export const ADD_SCORE = 'ADD_SCORE';
+
+export const addShip = (currentShip) => {
+  return {
+    type: ADD_SHIP,
+    piece: currentShip.piece,
+    pos: currentShip.pos,
+    index: currentShip.index,
+    row: currentShip.row,
+    col: currentShip.col
+  }
+}
+
+export const destroyEnemySpot = (row, col) => {
+  return {
+    type: DESTROY_SPOT,
+    board: enemyBoard,
+    row,
+    col
+  }
+}
+
+// export const addScore = () => {
+//   return {
+//     type: 
+//   }
+// }
+
+//exampleState
+
+// {
+//   gameState: 'Battle' || 'BeginGame',
+//   board: [[{piece: 'ACC1', pos: 'horizontal', hit: false}, {piece: 'ACC2', hit: false}]]
+// }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,67 @@
 import { combineReducers } from 'redux';
+import * as actions from '../actions/index.js'
+
 
 function gameStatus(state = 'pregame', action) {
     console.log('start game');
     return null
+}
+
+// function gameScore(state = {}, action) {
+//   switch (action.type) {
+//     case actions.ADD_SCORE:
+//       return Object.assign({}, state, {points: action.points + state.points})
+//   }
+// }
+const createBoard = () => {
+  const initialBoard = [];
+  for (let m = 0; m < 10; m++) {
+    let row = [];
+    for (let n = 0; n < 10; n++) {
+      row.push({piece: null, pos: null, hit: false});
+    }
+    initialBoard.push(row);
+  }
+  return initialBoard;
+}
+
+const initalState = {
+  gameState: 'BeginGame',
+  playerBoard: createBoard(),
+  enemyBoard: createBoard()
+}
+
+function gameLogic (state = initialState, action) {
+  switch (action.type) {
+    case ADD_SHIP:
+      return Object.assign({}, state, {
+        playerBoard:  state.playerBoard.map((row, rowIdx) => {
+          return row.map((ship, colIdx) => {
+            if (rowIdx === action.row && colIdx === action.col) {
+              return Object.assign({}, ship, {
+                piece: action.piece,
+                pos: action.pos,
+                hit: false
+              })
+            }
+            return ship;
+          })
+        })
+      })
+    case DESTROY_SPOT:
+      return Object.assign({}, state, {
+        enemyBoard: state.enemyBoard.map((row, rowIdx) => {
+          return row.map((ship, colIdx) => {
+            if (rowIdx === action.row && colIdx === action.col) {
+              return Object.assign({}, ship, {
+                hit: true
+              })
+            }
+            return ship;
+          })
+        })
+      })  
+  }
 }
 
 const BattleshipApp = combineReducers({
