e1e33fbcc1a48b94fa8ab75cf4e67b5da7083064
Jarrod Powell <jpowell1032@gmail.com>
2018-05-08 09:49:34 -0700

    Add redux-thunk middleware

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BattleshipGame from './components/BattleshipGame.jsx';
 import BattleshipApp from './reducers/index.js'
 
-const store = createStore(BattleshipApp);
+const store = createStore(
+  BattleshipApp,
+  applyMiddleware(thunk)
+  );
 
 const App = () => (
   <Provider store={store}>
