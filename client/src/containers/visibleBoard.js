b469bada277d3c7a6441ea8615fd74ea401bebab
Jarrod Powell <jpowell1032@gmail.com>
2018-05-22 13:27:15 -0700

    Improve rendering performance of board with selectors
    now board only re-renders when state of board changes
    now cursor updates only when state of selected ship and position changes

--- client/src/containers/visibleBoard.js
+++ client/src/containers/visibleBoard.js
@@ -1,11 +1,19 @@
 import { connect } from 'react-redux';
 import Board from '../components/Board.jsx';
 import { onCellClick } from '../actions/index.js';
+import { createSelector } from 'reselect';
 
-const getVisibleBoard = (board, boardType) => {
-  return board.map( (row, rowIdx) => {
-    return row.map( (spot, colIdx) => {
-      if (boardType === 'playerBoard') {
+const visiblePlayerBoard = (state) => state.gameLogic.playerBoard;
+const visibleEnemyBoard = (state) => state.gameLogic.enemyBoard;
+const getGamePhase = (state) => state.gamePhase;
+const getSelectedPiece = (state) => state.gameLogic.selectedPiece;
+const getSelectedPosition = (state) => state.gameLogic.selectedPosition;
+
+const getPlayerBoard = createSelector(
+  [visiblePlayerBoard],
+  (board) => {
+    return board.map( (row, rowIdx) => {
+      return row.map( (spot, colIdx) => {
         if (!spot.hit && spot.piece !== 'E') {
           return spot.piece;
         } else if (spot.hit && spot.piece === 'E') {
@@ -15,7 +23,16 @@ const getVisibleBoard = (board, boardType) => {
         } else {
           return '';
         }
-      } else {
+      })
+    })
+  }
+)
+
+const getEnemyBoard = createSelector(
+  [visibleEnemyBoard],
+  (board) => {
+    return board.map( (row, rowIdx) => {
+      return row.map( (spot, colIdx) => {
         if (!spot.hit) {
           return '';
         } else if (spot.hit && spot.piece === 'E') {
@@ -24,30 +41,36 @@ const getVisibleBoard = (board, boardType) => {
           return spot.piece;
         } else {
           return 'X'
-        }
-      }
+        }        
+      })
     })
-  })
-}
-
-const getCursorOnEnter = (gamePhase, selectedPiece, selectedPosition) => {
-  if (gamePhase === 'pregamePhase' && selectedPiece && selectedPosition) {
-    document.getElementsByClassName('playerBoard')[0].style.cursor = 'copy'
-  } else {
-    document.getElementsByClassName('playerBoard')[0].style.cursor = 'default'
   }
-}
+)
+
+const getCursorOnEnter = createSelector(
+  [getGamePhase, getSelectedPiece, getSelectedPosition],
+  (gamePhase, selectedPiece, selectedPosition) => {
+    console.log('phase', gamePhase);
+    console.log('piece', selectedPiece);
+    console.log('pos', selectedPosition);
+    if (gamePhase === 'pregamePhase' && selectedPiece && selectedPosition) {
+      document.getElementsByClassName('playerBoard')[0].style.cursor = 'copy'
+    } else {
+      document.getElementsByClassName('playerBoard')[0].style.cursor = 'default'
+    }     
+  }
+)
 
 const mapStateToProps = state => {
   console.log('state', state);
   const {selectedPiece, selectedPosition} = state.gameLogic;
   return {
-    playerBoard: getVisibleBoard(state.gameLogic.playerBoard, 'playerBoard'),
-    enemyBoard: getVisibleBoard(state.gameLogic.enemyBoard, 'enemyBoard'),
+    playerBoard: getPlayerBoard(state),
+    enemyBoard: getEnemyBoard(state),
     selectedPiece: selectedPiece,
     selectedPos: selectedPosition,
     gamePhase: state.gamePhase,
-    getCursorOnEnter: () => getCursorOnEnter(state.gamePhase, selectedPiece, selectedPosition)
+    getCursorOnEnter: () => getCursorOnEnter(state)
   }
 }
 
